89706f7673fc93f75b75c6ab1d9a6522e87dfda8
Madaffadl <67999188+Madaffadl@users.noreply.github.com>
2025-08-06 10:47:50 +0700

    Updated mockData.ts

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -86,7 +86,7 @@ export const mockClasses: Class[] = [
     // }
     location: {
       latitude: -6.9663,  // Latitude untuk Kota Semarang
-      longitude: 110.4203 // Longitude untuk Kota SemarangÏ
+      longitude: 110.4203, // Longitude untuk Kota SemarangÏ
       radius: 500
     }
   }
