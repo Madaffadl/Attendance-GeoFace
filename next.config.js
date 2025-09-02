cebf2967a8b74713fa154fbdac2cfee4abdb783e
Madaffadl <67999188+Madaffadl@users.noreply.github.com>
2025-09-02 23:12:41 +0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  experimental: {
+    // Disable experimental features if any
+    asyncServerComponents: false,
+    // other experimental features can be added here
+  },
 };
 
 module.exports = nextConfig;
